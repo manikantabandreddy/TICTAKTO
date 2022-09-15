149d91e84d1f58936b718c3c72c0a9a8b5bbb6c3
manikantabandreddy <109576597+manikantabandreddy@users.noreply.github.com>
2022-09-15 12:47:56 +0530

    Add files via upload

---
+++ bodmas.py
@@ -0,0 +1,30 @@
+x = input("enter the first number : ")
+y = input("enter the second number: ")
+
+if x.isnumeric and y.isnumeric():
+    a=int(x)
+    b=int(y)
+    var = input("enter the operations : +,-,*,%,/")
+    print("this is numeric")
+    if var == "+":
+        print("ADDITION : ",a+b)
+    elif var == "-":
+        print("SUBSTRACTION : ",a-b)
+    elif var == "*":
+        print("MULTIPLAY : ",a*b)
+    elif var == "%":
+        print("DIVISION : ",a%b)
+    elif var == "/":
+        print("REMINDER : ",a/b)
+        
+elif x.isalpha or y.isalpha:
+    print("you enter wrong input")
+    
+else:
+    print("THIS IS AN ALPHABATE")
+    
+    
+
+
+    
+    
